2842662bf39893619c2ef2c2c9e1d58daa8543ff
Geoffroy Perriard <geoffroy.perriard@liip.ch>
2016-11-12 15:20:04 +0100

    Put logic into functions

--- range_sensor.py
+++ range_sensor.py
@@ -1,7 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import pyglet
-
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 4
@@ -10,20 +8,36 @@ PIR = 2
 
 print "Distance Measurement In Progress"
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
-GPIO.setup(PIR,GPIO.IN)
+GPIO.setup(PIR,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
 
-sound2 = pyglet.media.load('sound2.mp3', streaming=False)
-sound2.play()
+GPIO.wait_for_edge(PIR, GPIO.RISING, callback=detected_people)
 
+detected = False
 
 while True:
 
-    print(GPIO.input(PIR))
+    if detected :
+        read_distance()
+        print "Detected OK"
+        detected = False
+
+    time.sleep(2)
+    
+
+GPIO.cleanup()
+
+def detected_people(PIR):
+    detected = True
+
+
+def read_distance():
+    
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
@@ -34,15 +48,10 @@ while True:
     while GPIO.input(ECHO)==1:
         pulse_end = time.time()
 
-    
     pulse_duration = pulse_end - pulse_start
-    
     distance = pulse_duration * 17150
-    
     distance = round(distance, 2)
-    
     print "Distance:",distance,"cm"
-    
-    time.sleep(2)
 
-GPIO.cleanup()
+    return distance
+
